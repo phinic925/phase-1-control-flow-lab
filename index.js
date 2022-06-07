f09700e3bbc2a86bbc119441befeb2101d23ee36
phinic925 <phinicnyangate480@gmail.com>
2022-06-07 16:36:33 +0100

    commit tree 0c51be8740536a31bd21b334a133b474205fdfa2

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+  if(someValue<=400){
+    return "This one is on me!"
+  }
+  else if (someValue>2500){
+    return "No can do."
+  }
+  else if (someValue>2000){
+    return "I will gladly take your thirty bucks."
+  }
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
-}
+ 
+    {return city === "NYC" ? "Ok, sounds good.": "No go."}
+ }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  if (tip === 'generous'){
+    return 'Thank you so much.'
+
+  }
+  else if (tip === 'not as generous'){
+    return 'Thank you.'
+  }
+  else {
+    return 'Bye.'
+  }
 }
